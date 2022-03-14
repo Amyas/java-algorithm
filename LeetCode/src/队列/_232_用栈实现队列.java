c3174a4907302b194d72698f1603610f1c763cd8
wangjianpeng <wangjianpeng@weimiao.cn>
2022-03-14 14:46:27 +0800

    05队列

---
+++ LeetCode/src/队列/_232_用栈实现队列.java
@@ -0,0 +1,41 @@
+package 队列;
+
+import java.util.Stack;
+
+public class _232_用栈实现队列 {
+  private Stack<Integer> inStack;
+  private Stack<Integer> outStack;
+
+  public _232_用栈实现队列() {
+    inStack = new Stack<>();
+    outStack = new Stack<>();
+  }
+
+  public void push(int x) {
+    inStack.push(x);
+  }
+
+  public int pop() {
+    if (outStack.isEmpty()) {
+      while (!inStack.isEmpty()) {
+        outStack.push(inStack.pop());
+      }
+    }
+
+    return outStack.pop();
+  }
+
+  public int peek() {
+    if (outStack.isEmpty()) {
+      while (!inStack.isEmpty()) {
+        outStack.push(inStack.pop());
+      }
+    }
+
+    return outStack.peek();
+  }
+
+  public boolean empty() {
+    return inStack.isEmpty() && outStack.isEmpty();
+  }
+}
